bc92d10ad770a0fe306453b92fbf60f383753401
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-01-05 02:46:03 -0700

    random algorithm

---
+++ runs/run_random.py
@@ -0,0 +1,20 @@
+from simulator.env import Env
+import random
+
+def run_random():
+    env = Env()
+    env.reset()
+    #print(env.show_graph())
+    #print(env.show_drivers_in_spatial())
+    directions = [i for i in range(10)]
+    for _ in range(30):
+        actions = [random.choice(directions) for _ in range(env.get_drivers_length())]
+        env.step(actions)
+        #print(env.show_riders_in_spatial())
+        #print(env.show_drivers_in_spatial())
+    print(env.show_metrics_in_spatial())
+    print(env.show_metrics_each_driver())
+    print(env.show_metrics_in_summary())
+
+if __name__ == "__main__":
+    run_random()
