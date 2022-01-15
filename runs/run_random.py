6365579b74a2db30a6e1e5d3c169c2f222574203
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-01-14 22:14:19 -0700

    update random strategy

--- runs/run_random.py
+++ runs/run_random.py
@@ -14,13 +14,10 @@ def run_random():
         while not done:
             actions = [random.choice(directions) for _ in range(env.get_drivers_length())]
             _, _, done, _ = env.step(actions)
+            if i_step % 1000 == 0:
+                print(env.show_metrics_in_summary())
             i_step += 1
-            #print(actions[7])
-            #print(env.monitor_drivers[7])
-            #print(env.show_riders_in_spatial())
-            #print(env.show_drivers_in_spatial())
-        #print(env.show_metrics_in_spatial())
-        #print(env.show_metrics_each_driver())
+        print("Episode end:\n")
         print(env.show_metrics_in_summary())
 
 if __name__ == "__main__":
