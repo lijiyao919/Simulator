a0cf94751c013c504875b7d60a0ab1036e015079
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-01-16 01:47:13 -0700

    update

--- runs/run_random.py
+++ runs/run_random.py
@@ -14,8 +14,11 @@ def run_random():
         while not done:
             actions = [random.choice(directions) for _ in range(env.get_drivers_length())]
             _, _, done, _ = env.step(actions)
-            if i_step % 1000 == 0:
-                print(env.show_metrics_in_summary())
+            #if i_step % 1000 == 0:
+                #print(actions)
+                #print(env.show_fail_riders_num_in_spatial())
+                #print(env.show_offline_driver_status_in_specific_zone(9))
+                #print(env.show_metrics_in_summary())
             i_step += 1
         print("Episode end:\n")
         print(env.show_metrics_in_summary())
