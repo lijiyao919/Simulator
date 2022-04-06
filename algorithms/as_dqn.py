201597488c57bffbaa3bdfd7e686c07b4420deaf
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-04-06 14:29:01 -0600

    add as-dqn

---
+++ algorithms/as_dqn.py
@@ -0,0 +1,57 @@
+from algorithms.idqn import IDQN_Agent
+from algorithms.idqn import device
+from simulator.timer import Timer
+import torch.nn.functional as F
+import torch as T
+import numpy as np
+import random
+from torch.distributions import Categorical
+
+SAMPLE="rank"
+
+class AS_DQN_Agent(IDQN_Agent):
+    def __init__(self, input_dims, n_actions, fc1_dims, eta, buffer_size=10000, batch_size=32, gamma=0.99, target_update_feq=1000, eps_end=0.1, eps_decay=1000000):
+        super().__init__(input_dims, n_actions, fc1_dims, eta, buffer_size, batch_size, gamma, target_update_feq, eps_end, eps_decay)
+
+    def _prob_func(self, arr, tao):
+        for i in range(len(arr)):
+            arr[i] = pow(1/arr[i], tao)
+        all_sum = arr.sum()
+        for i in range(len(arr)):
+            arr[i] = arr[i]/all_sum
+
+    def select_action(self, obs, drivers, steps_done):
+        actions = [-1] * len(drivers)
+        time = Timer.get_time(Timer.get_time_step())
+        day = Timer.get_day(Timer.get_time_step())
+        assert 0 <= time <= 1440
+        assert 1 <= day <= 7
+
+        random_num = random.random()
+        eps_thredhold = 0.1 #self.eps_end + (self.eps_start-self.eps_end)*math.exp(-1 * steps_done / self.eps_decay)
+        for did, driver in drivers.items():
+            if driver.on_line is True:
+                assert obs["driver_locs"][did] == driver.zid
+                if random_num > eps_thredhold:
+                    with T.no_grad():
+                        state = IDQN_Agent._get_state(time, day, driver.zid)
+                        state_tensor = T.from_numpy(np.expand_dims(state.astype(np.float32), axis=0)).to(device)
+                        if SAMPLE == "softmax":
+                            #print("softmax")
+                            probs = F.softmax(self.policy_net(state_tensor), dim=1)
+                        elif SAMPLE =="rank":
+                            #print("rank")
+                            sort_idx = (-self.policy_net(state_tensor)).argsort(dim=1)
+                            ranks = T.empty_like(sort_idx, dtype=T.float32)
+                            ranks[0][sort_idx] = T.arange(1, len(sort_idx[0])+1, device=device, dtype=T.float32)
+                            self._prob_func(ranks[0], 3)
+                            probs = ranks
+                        else:
+                            raise Exception("No sampling type")
+                        m = Categorical(probs)
+                        action = m.sample()
+                        actions[did] = action.item()
+                else:
+                    actions[did] = random.randrange(self.n_actions)
+        return actions
+
---
+++ runs/run_as_dqn.py
@@ -0,0 +1,45 @@
+from simulator.env import Env
+from simulator.timer import Timer
+from simulator.objects import Reward_ICAART, Reward_Distribution
+from simulator.config import *
+from simulator.monitor import Monitor
+from algorithms.as_dqn import AS_DQN_Agent
+
+RUN_STEP = 1027180
+
+def run_ds_dqn():
+    env = Env()
+    env.set_reward_scheme(Reward_ICAART())
+    agent = AS_DQN_Agent(1524, 10, 256, 0.0001)
+    agent.train_mode()
+    i_step = 0
+
+    while i_step < RUN_STEP:
+        obs = env.reset()
+        done = False
+        while not done:
+            if Timer.get_time_step() != 0 and Timer.get_time_step() % TOTAL_MINUTES_ONE_DAY == 0:
+                '''print("The current step: ", i_step)
+                print("The current time stamp: ", Timer.get_time_step())
+                print("The current date: ", Timer.get_date(Timer.get_time_step()))
+                print(env.show_metrics_in_summary())
+                if ON_MONITOR:
+                    Monitor.reset_by_time()'''
+            actions = agent.select_action(obs, env.monitor_drivers, i_step)
+            next_obs, rewards, done, _ = env.step(actions)
+            if ON_MONITOR:
+                Monitor.reset_by_zone()
+            agent.store_exp(env.monitor_drivers, obs, actions, rewards, next_obs)
+            agent.update(i_step)
+            obs = next_obs
+            i_step += 1
+        #print("save checkpoint")
+        #agent.policy_net.save_checkpoint()
+        print("Episode end:")
+        print("The current time stamp: ", Timer.get_time_step())
+        print("The current step: ", i_step)
+        print(env.show_metrics_in_summary())
+
+
+if __name__ == '__main__':
+    run_ds_dqn()
