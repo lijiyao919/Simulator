eb015333bc6a48111fe5ba8436ad4a4b2f5e36d6
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-07-23 16:13:55 -0600

    remove reward_distribution

--- algorithms/driver_base/rewards.py
+++ algorithms/driver_base/rewards.py
@@ -19,18 +19,6 @@ class Reward_ICAART(Reward):
             raise Exception("wrong reward")
 
 #from ITSC2022 paper
-class Reward_Distribution(Reward):
-    def reward_scheme(self, driver, obs):
-        if driver.in_service is True:
-            assert driver.rider is not None
-            return (PATIENCE_TIME - driver.rider.call_taxi_duration)
-        elif driver.on_line is True:
-            return -1
-        elif driver.on_line is False:
-            return -2
-        else:
-            raise Exception("wrong reward")
-
 class Reward_Distribution_v2(Reward):
     def reward_scheme(self, driver, obs):
         if driver.in_service is True:
