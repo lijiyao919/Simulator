d94a43b300e8f5ecea037dca0c1b85e7d469eaac
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-10-11 01:22:58 -0600

    add dueling structure

--- algorithms/models/mlp_net.py
+++ algorithms/models/mlp_net.py
@@ -10,14 +10,11 @@ class MLP_Network(nn.Module):
     def __init__(self, input_dims, n_actions, fc1_dims, eta, tb_writer, chkpt_dir='../checkpoints', chkpt_file='mlp_nwk.pth'):
         super(MLP_Network, self).__init__()
         self.fc1 = nn.Linear(input_dims, fc1_dims)
+        self.fc1_pi = nn.Linear(input_dims, fc1_dims)
         self.elu = nn.ELU()
-        if DUELING:
-            self.fc2_a = nn.Linear(fc1_dims, 64)
-            self.fc2_b = nn.Linear(fc1_dims, 64)
-            self.fc3_a = nn.Linear(64, 1)
-            self.fc3_b = nn.Linear(64, n_actions)
-        else:
-            self.fc3 = nn.Linear(fc1_dims, n_actions)
+        self.tanh = nn.Tanh()
+        self.fc3 = nn.Linear(fc1_dims, n_actions)
+        self.fc2 = nn.Linear(fc1_dims, 1)
 
         self.optimizer = optim.RMSprop(self.parameters(), lr=eta)    #0.0001
         # self.optimizer = optim.SGD(self.__policy.parameters(), lr=0.0001)  # 0.01 for method2, 3, online learn
@@ -27,16 +24,17 @@ class MLP_Network(nn.Module):
         self.writer = tb_writer
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.elu(x)
         if DUELING:
-            x_a = self.elu(self.fc2_a(x))
-            x_b = self.elu(self.fc2_b(x))
-            V = self.fc3_a(x_a)
-            A = self.fc3_b(x_b)
+            x_a = self.tanh(self.fc1(x))
+            x_b = self.tanh(self.fc1_pi(x))
+            V = self.fc2(x_a)
+            A = self.fc3(x_b)
             AVER_A = T.mean(A, dim=1, keepdim=True)
             return V + (A - AVER_A)
         else:
+            x = self.fc1(x)
+            #x = self.elu(x)
+            x = self.tanh(x)
             action_scores = self.fc3(x)
             return action_scores
 
