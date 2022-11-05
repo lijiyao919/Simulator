39fc9ebd7f5a97aa7794d8de5c7413a49760ffb9
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-11-05 04:08:05 -0600

    fix clean and add new data 9/17/2019

--- data/clean.py
+++ data/clean.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import random
 
-FILE_NAME = "Taxi_Trips_Chicago_08_2019.csv"
-CLEAN_FILE_NAME = "Taxi_Trips_Chicago_08_2019_clean.csv"
+FILE_NAME = "Taxi_Trips_Chicago_7AM_to_11AM_09_17_2019.csv"
+CLEAN_FILE_NAME = "Taxi_Trips_Chicago_7AM_to_11AM_09_17_2019_clean.csv"
 YEAR = 2019
-MONTH = 8
-DATE = 1
+MONTH = 9
+DATE = 17
 TOTAL_TIME_STEP_ONE_EPISODE = 44640
 
 def clean_trips_data():
@@ -27,7 +27,7 @@ def clean_trips_data():
         d["ID"].append(actor_id)
 
         #time
-        timestamp = int((pd.to_datetime(obs["Trip Start Timestamp"]) - pd.Timestamp(YEAR, MONTH, DATE, 0)) / pd.to_timedelta(1, unit='m'))
+        timestamp = int((pd.to_datetime(obs["Trip Start Timestamp"]) - pd.Timestamp(YEAR, MONTH, DATE, 7)) / pd.to_timedelta(1, unit='m'))
         timestamp = timestamp + round(random.uniform(0, 15))
         assert 0<=timestamp<=TOTAL_TIME_STEP_ONE_EPISODE
         d["Time"].append(timestamp)
@@ -41,12 +41,12 @@ def clean_trips_data():
         d["Dropoff"].append(dropoff_zone)
 
         #Duration
-        trip_duration_in_minute = 1 if int(float(obs["Trip Seconds"])/60)==0 else int(float(obs["Trip Seconds"])/60)
+        trip_duration_in_minute = 1 if int(float(obs["Trip Seconds"].replace(',', ''))/60)==0 else int(float(obs["Trip Seconds"].replace(',', ''))/60)
         assert trip_duration_in_minute != 0
         d["Duration"].append(trip_duration_in_minute)
 
         #Price
-        d["Fare"].append(float(obs["Trip Total"]))
+        d["Fare"].append(float(obs["Trip Total"].replace(',', '')))
 
     print("Write data...")
     df = pd.DataFrame(d)
