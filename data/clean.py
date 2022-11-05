10e3de2338b66f96ab47894c777a05ce6ecfd7d4
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-11-05 12:52:49 -0600

    update clearn and add 11am to 3pm and 5pm to 9pm

--- data/clean.py
+++ data/clean.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import random
 
-FILE_NAME = "Taxi_Trips_Chicago_7AM_to_11AM_09_17_2019.csv"
-CLEAN_FILE_NAME = "Taxi_Trips_Chicago_7AM_to_11AM_09_17_2019_clean.csv"
+FILE_NAME = "Taxi_Trips_Chicago_5PM_to_9PM_09_17_2019.csv"
+CLEAN_FILE_NAME = "Taxi_Trips_Chicago_5PM_to_9PM_09_17_2019_clean.csv"
 YEAR = 2019
 MONTH = 9
 DATE = 17
-TOTAL_TIME_STEP_ONE_EPISODE = 44640
+TIME_HOUR = 17
+TOTAL_TIME_STEP_ONE_EPISODE = 250
 
 def clean_trips_data():
     df = pd.read_csv(FILE_NAME)
@@ -27,7 +28,7 @@ def clean_trips_data():
         d["ID"].append(actor_id)
 
         #time
-        timestamp = int((pd.to_datetime(obs["Trip Start Timestamp"]) - pd.Timestamp(YEAR, MONTH, DATE, 7)) / pd.to_timedelta(1, unit='m'))
+        timestamp = int((pd.to_datetime(obs["Trip Start Timestamp"]) - pd.Timestamp(YEAR, MONTH, DATE, TIME_HOUR)) / pd.to_timedelta(1, unit='m'))
         timestamp = timestamp + round(random.uniform(0, 15))
         assert 0<=timestamp<=TOTAL_TIME_STEP_ONE_EPISODE
         d["Time"].append(timestamp)
@@ -46,7 +47,10 @@ def clean_trips_data():
         d["Duration"].append(trip_duration_in_minute)
 
         #Price
-        d["Fare"].append(float(obs["Trip Total"].replace(',', '')))
+        if isinstance(obs["Trip Total"], float):
+            d["Fare"].append(obs["Trip Total"])
+        else:
+            d["Fare"].append(float(obs["Trip Total"].replace(',', '')))
 
     print("Write data...")
     df = pd.DataFrame(d)
