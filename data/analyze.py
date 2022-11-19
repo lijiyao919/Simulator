409f507f64cf7152c00cb6533f2265b5871e8892
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-11-19 15:00:10 -0700

    add spatio to analyze

--- data/analyze.py
+++ data/analyze.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from collections import defaultdict
 import matplotlib.pylab as plt
+from simulator.config import *
 import numpy as np
 
-FILE_NAME = "Taxi_Trips_Chicago_5PM_to_9PM_09_17_2019_clean.csv"
+FILE_NAME = "Taxi_Trips_Chicago_7AM_to_11AM_09_17_2019_clean.csv"
 TOTAL_TIME_STEP_ONE_EPISODE = 240
 mean_range =15
 
@@ -11,6 +12,7 @@ def analyze_data():
     df = pd.read_csv(FILE_NAME)
     demand = defaultdict(lambda : 0)
     duration = defaultdict(lambda: 0)
+    spatio = defaultdict(lambda : [0]*(TOTAL_ZONES+1))
 
     print("Importing Trip Data...")
     for i in range(len(df)):
@@ -24,6 +26,10 @@ def analyze_data():
         trip_duration_in_minute = int(obs["Duration"])
         duration[trip_duration_in_minute]+=1
 
+        #spatio
+        spatio[int(timestamp/10+1)][int(obs["Pickup"])] += 1
+
+    #time
     plt.figure(0)
     lists = sorted(demand.items())  # sorted by key, return a list of tuples
     x, y = zip(*lists)  # unpack a list of pairs into two tuples
@@ -38,11 +44,23 @@ def analyze_data():
     plt.ylabel("The Number of Demand")
     plt.show()
 
-    '''plt.figure(1)
+    #duration
+    plt.figure(1)
     lists = sorted(duration.items())  # sorted by key, return a list of tuples
     x, y = zip(*lists)  # unpack a list of pairs into two tuples
     plt.bar(x, y)
-    plt.show()'''
+    plt.show()
+
+    #spatio
+    for t, dist in spatio.items():
+        plt.figure(2)
+        plt.title(t*10)
+        plt.bar(range(TOTAL_ZONES+1), dist)
+        plt.xticks(range(0, TOTAL_ZONES+1, 2))
+        plt.xlabel("Zone")
+        figManager = plt.get_current_fig_manager()
+        figManager.window.showMaximized()
+        plt.show()
 
 
 
