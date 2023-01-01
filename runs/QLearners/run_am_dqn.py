ebd3ecb6437fb4c1f1402c6e8a78bfc0ce7e8dc4
Jiyao Li <jiyao.thomas.li@gmail.com>
2022-12-31 18:12:20 -0700

    update am-dqn

--- runs/QLearners/run_am_dqn.py
+++ runs/QLearners/run_am_dqn.py
@@ -2,16 +2,15 @@ from simulator.env import Env
 from simulator.timer import Timer
 from algorithms.QLearners.rewards import Reward_Distribution_v2
 from simulator.config import *
-from simulator.monitor import Monitor
 from algorithms.QLearners.am_dqn import AM_DQN_Agent
 
 RUN_STEP = 3027180
 
 def run_am_dqn():
     env = Env()
-    agent = AM_DQN_Agent(308, 10, 128, 0.000001, target_update_feq=10000, buffer_size=100000, batch_size=32)
+    #agent = AM_DQN_Agent(308, 10, 128, 0.000001, target_update_feq=10000, buffer_size=100000, batch_size=32)
+    agent = AM_DQN_Agent(539, 10, 256, 0.0001)
     agent.set_reward_scheme(Reward_Distribution_v2())
-    #agent = AM_DQN_Agent(1524, 10, 256, 0.0001)
     agent.train_mode()
     i_step = 0
 
