b8fd0adb8bbdc5638e45af8c6900c4eeeb5d431e
Jiyao Li <jiyao.thomas.li@gmail.com>
2023-01-05 13:49:42 -0700

    update am-dqn run

--- runs/QLearners/run_am_dqn.py
+++ runs/QLearners/run_am_dqn.py
@@ -9,7 +9,7 @@ RUN_STEP = 3027180
 def run_am_dqn():
     env = Env()
     #agent = AM_DQN_Agent(308, 10, 128, 0.000001, target_update_feq=10000, buffer_size=100000, batch_size=32)
-    agent = AM_DQN_Agent(539, 10, 256, 0.0001)
+    agent = AM_DQN_Agent(539, 10, 256, 0.0001, target_update_feq=10000, buffer_size=100000, batch_size=32)
     agent.set_reward_scheme(Reward_Distribution_v2())
     agent.train_mode()
     i_step = 0
@@ -18,13 +18,6 @@ def run_am_dqn():
         obs = env.reset()
         done = False
         while not done:
-            '''if Timer.get_time_step() != 0 and Timer.get_time_step() % TOTAL_MINUTES_ONE_DAY == 0:
-                print("The current step: ", i_step)
-                print("The current time stamp: ", Timer.get_time_step())
-                print("The current date: ", Timer.get_date(Timer.get_time_step()))
-                print(env.show_metrics_in_summary())
-                if ON_MONITOR:
-                    Monitor.reset_by_time()'''
             actions = agent.select_action(obs, env.monitor_drivers, i_step)
             next_obs, _, done, _ = env.step(actions)
             rewards = agent.iterate_drivers_reward(env.monitor_drivers, actions)
