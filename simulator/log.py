85c3ae8a74aedcde15a6a49ca558f758c57b4993
Jiyao Li <jiyao.thomas.li@gmail.com>
2023-04-19 22:38:07 -0600

    add log module

---
+++ simulator/log.py
@@ -0,0 +1,37 @@
+from simulator.config import *
+
+class EpisodeFilter(logging.Filter):
+    def __init__(self, ep_thred):
+        self._ep_thred = ep_thred
+
+    def filter(self, record):
+        msg = record.getMessage()
+        ep = int(msg[0])
+
+        if ep >= self._ep_thred:
+            return True
+        else:
+            return False
+
+class MyLogger:
+    def __init__(self, logger_name):
+        #create a logger
+        self._logger = logging.getLogger(logger_name)
+
+        #create a handler
+        formatter = logging.Formatter("%(levelname)s - %(name)s - %(message)s")
+        file_handler = logging.FileHandler(LOG_FILE, mode='w', encoding='utf-8')
+        file_handler.setFormatter(formatter)
+
+        #create a filter
+        ep_f = EpisodeFilter(LOG_EP_LEVEL)
+
+        # set up logger
+        self._logger.setLevel(LOG_LEVEL)
+        self._logger.addHandler(file_handler)
+        self._logger.addFilter(ep_f)
+        self.propagate = False
+
+    def get_logger(self):
+        return self._logger
+
