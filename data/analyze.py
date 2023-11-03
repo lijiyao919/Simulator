8d652a66925310ac269e61471929626fd3aac9fc
Jiyao Li <jiyao.thomas.li@gmail.com>
2023-11-03 11:35:54 -0600

    show city and downtown pattern and downtown percentage

--- data/analyze.py
+++ data/analyze.py
@@ -6,11 +6,12 @@ import random
 
 FILE_NAME = "Taxi_Trips_Chicago_08_29_2019.csv"
 TOTAL_TIME_STEP_ONE_EPISODE = 1440
-mean_range =15
+mean_range = 7
 
 def analyze_data():
     df = pd.read_csv(FILE_NAME)
-    demand = defaultdict(lambda : 0)
+    demand_city = [0 for _ in range(1441)]
+    demand_dt = [0 for _ in range(1441)]
 
     print("Importing Trip Data...")
     for i in range(len(df)):
@@ -19,22 +20,45 @@ def analyze_data():
         #time
         timestamp = int((pd.to_datetime(obs["Trip Start Timestamp"]) - pd.Timestamp(2019, 8, 29, 0)) / pd.to_timedelta(1, unit='m'))
         timestamp = timestamp + round(random.uniform(0, 15))
-        demand[timestamp]+=1
+        #if int(obs["Pickup Community Area"]) == 76: #airport
+        if int(obs["Pickup Community Area"]) == 8 or int(obs["Pickup Community Area"]) == 28 or \
+            int(obs["Pickup Community Area"]) == 32 or int(obs["Pickup Community Area"]) == 33: #downtown
+                demand_dt[timestamp] += 1
+        demand_city[timestamp]+=1 # city
+
+    z_city = [0 for _ in range(0, 1440)]
+    for i in range(0, 1440):
+        #print(i, len(demand_city[max(0,i-mean_range): min(i+mean_range+1, 1440)]))
+        z_city[i] = sum(demand_city[max(0,i-mean_range): min(i+mean_range+1,1440)]) / len(demand_city[max(0,i-mean_range): min(i+mean_range+1, 1440)])
+
+
+    z_dt = [0 for _ in range(0, 1440)]
+    for i in range(0, 1440):
+        #print(i, len(demand_dt[max(0, i - mean_range): min(i + mean_range + 1, 1440)]))
+        z_dt[i] = sum(demand_dt[max(0, i - mean_range): min(i + mean_range + 1, 1440)]) / len(demand_dt[max(0, i - mean_range): min(i + mean_range + 1, 1440)])
+
+    x = range(0, 1440)
 
     plt.figure(0)
-    lists = sorted(demand.items())  # sorted by key, return a list of tuples
-    x, y = zip(*lists)  # unpack a list of pairs into two tuples
-    z = [0 for _ in range(len(y))]
-    for i in range(len(y)):
-        z[i] = sum(y[max(0,i-mean_range): min(len(y),i+mean_range+1)])/len(y[max(0,i-mean_range): min(len(y),i+mean_range+1)])
-
     plt.rc('font', size=12)
-    #plt.plot(x, y)
-    plt.plot(x, z, linewidth=3)
+    plt.plot(x, z_city, linewidth=3, label="Whole City")
+    plt.plot(x, z_dt, linewidth=3, label="Downtown")
     plt.xticks(range(0, 1441, 60), range(0, 25, 1), fontsize=18)
     plt.yticks(fontsize=18)
     plt.xlabel("Time (hours)", fontsize=22)
-    plt.ylabel("The Number of Requests", fontsize=22)
+    plt.ylabel(f"The Number of Requests per Minute", fontsize=22)
+    plt.legend()
+    plt.grid(linestyle='--')
+    plt.show()
+
+    z_city = np.array(z_city)
+    z_dt = np.array(z_dt)
+    plt.plot(x, z_dt/z_city, linewidth=3)
+    plt.xticks(range(0, 1441, 60), range(0, 25, 1), fontsize=18)
+    plt.yticks(np.arange(0.4, 1, 0.1), range(40, 100, 10), fontsize=18)
+    plt.xlabel("Time (hours)", fontsize=22)
+    plt.ylabel("Percentage (%)", fontsize=22)
+    plt.grid(linestyle='--')
     plt.show()
 
 if __name__ == "__main__":
